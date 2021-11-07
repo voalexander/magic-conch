aa03ea30085261a99fcf2da4051648a01e3cc670
voalexander <alexhovo@gmail.com>
2021-11-07 16:54:26 -0600

    Updates

---
+++ precache-manifest.6494157f6a2806b54679ae4440ba378b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95fd1b3631a37887455068f92491e7d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e89aa1252c1d94c996e",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "bb40750293e4ed4c73d5",
+    "url": "/static/js/2.60618335.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.60618335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e89aa1252c1d94c996e",
+    "url": "/static/js/main.b7890f3e.chunk.js"
+  },
+  {
+    "revision": "d6a81d3ddc3cac50cbaf",
+    "url": "/static/js/runtime-main.b1333238.js"
+  },
+  {
+    "revision": "b4d2d475593ef0df946899ad2357757d",
+    "url": "/static/media/base.b4d2d475.jpg"
+  },
+  {
+    "revision": "c9c916e655b92e7c7035e852e20b146c",
+    "url": "/static/media/idontthinkso1.c9c916e6.mp3"
+  },
+  {
+    "revision": "bee6ff4a016eeb0309eaa144be7b650b",
+    "url": "/static/media/maybesomeday1.bee6ff4a.mp3"
+  },
+  {
+    "revision": "2d2fcbe40f8b7ee9bdcd4066d5735526",
+    "url": "/static/media/no1.2d2fcbe4.mp3"
+  },
+  {
+    "revision": "51a8ae7476f1ea14a9f37908c62b1a39",
+    "url": "/static/media/no2.51a8ae74.mp3"
+  },
+  {
+    "revision": "fe1864ed7db9e950d992723b79892de7",
+    "url": "/static/media/no3.fe1864ed.mp3"
+  },
+  {
+    "revision": "a9a44dc861616e984268c091fed11cf1",
+    "url": "/static/media/tryaskingagain1.a9a44dc8.mp3"
+  },
+  {
+    "revision": "6812b6d2c7099806956c02d6f16f3eaa",
+    "url": "/static/media/yes1.6812b6d2.mp3"
+  }
+]);
